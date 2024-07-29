45752b30b757950ecfdcd6b875a06905d5442602
alyaafiif <alyaafifkurnia@gmail.com>
2024-07-29 09:21:21 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,16 @@
+def luas_segitiga():
+    a = int(input("Masukkan alas segitiga: "))
+    t = int(input("Masukkan tinggi segitiga:"))
+    luas = a * t / 2
+    print("Luas segitiga adalah: ", luas)
+
+luas_segitiga()
+
+
+def luas_persegi_panjang():
+    p = int(input("Masukkan panjang persegi:"))
+    l = int(input("Masukkan lebar persegi:"))
+    luas = p * l
+    print("Luas persegi adalah: ", luas)
+
+luas_persegi_panjang()
